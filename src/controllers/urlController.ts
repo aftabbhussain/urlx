7105e019be0d17816e40bc72a6d1caec4a37bc06
aftabbhussain <88221488+aftabbhussain@users.noreply.github.com>
2026-05-12 16:02:45 +0530

    Organize the codebase into a Controller-Router Architecture Added the appropriate urlControllers and urlRoutes for seamless working of the API

---
+++ src/controllers/urlController.ts
@@ -0,0 +1,68 @@
+import { UrlModel } from "../models/Url";
+import { Request, Response } from "express";
+import { generateId } from "../services/idGenerator";
+
+export const shortenUrl = async (req: Request, res: Response) => {
+    const longUrl = req.body.longUrl;
+    if(!longUrl){
+        return res.status(400).json({
+            error: "longUrl is required"
+        });
+    }
+
+    try{
+        new URL(longUrl); //check if the given url is valid or not
+    }
+    catch(error){
+        return res.status(400).json({
+            error: "Invalid Url"
+        });
+    }
+
+    
+    try{
+        const existingUrl = await UrlModel.findOne({longUrl: longUrl});
+        if(existingUrl){
+            return res.status(200).json({shortId: existingUrl.shortId});
+        }
+        //if the long url doesn't exist, we create one
+        const shortId = generateId(5);
+        const newUrl = await UrlModel.create({
+            longUrl: longUrl,
+            shortId: shortId
+        });
+        return res.status(201).json({
+            message: "ShortId created successfully",
+            data: newUrl
+        })
+
+
+    }
+    catch(error){
+        return res.status(500).json({
+            error: "Internal server error"
+        })
+    };
+}
+
+//function to redirect url
+export const redirectUrl = async (req: Request, res: Response) => {
+    const shortId = req.params.shortId;
+    try{
+        const existingUrl = await UrlModel.findOne({shortId: shortId});
+        if(existingUrl){
+            return res.status(200).json({
+                longUrl: existingUrl.longUrl
+            });
+        }
+        return res.status(404).json({
+            error: "Url not found"
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            error: "Internal server error"
+        })
+    }
+
+}
---
+++ src/routes/urlRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express"; 
+import { shortenUrl, redirectUrl } from "../controllers/urlController";
+const router = Router();
+
+router.post('/shorten', shortenUrl);
+router.get('/:shortId', redirectUrl);
+
+export default router;
